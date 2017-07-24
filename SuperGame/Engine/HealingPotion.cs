563802feffd44909f9901ce2c9bab14c95c4e90f
hard_system <hard_system@abv.bg>
2017-07-24 13:31:44 +0100

    New Version
    Here I added better version of the code!

---
+++ SuperGame/Engine/HealingPotion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public class HealingPotion : Item
+    {
+       
+        public int AmountToHeal { get; set; }
+        // : base() - takes all the parameters from the Item because we inherite from Item(it is our base) 
+        public HealingPotion(int id, string name, string namePlural, int amountToHeal) : base(id, name, namePlural)
+        {
+            AmountToHeal = amountToHeal;
+        }
+    }
+}
